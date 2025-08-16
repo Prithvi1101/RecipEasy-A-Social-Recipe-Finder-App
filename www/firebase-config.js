086210461f63bfaab86b3b2c19320a514a1a721f
Prithviraj Chowdhury <132608501+Prithvi1101@users.noreply.github.com>
2025-08-16 17:40:09 +0600

    Add files via upload

---
+++ www/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "YOUR GOOGLE API KEY",
+  authDomain: "recipeasy-c3c16.firebaseapp.com",
+  projectId: "recipeasy-c3c16",
+  storageBucket: "recipeasy-c3c16.appspot.com",
+  messagingSenderId: "555770501005",
+  appId: "1:555770501005:web:0e8e799e0abd04cd030fc4"
+};
+
+// ✅ COMPAT mode initialization
+firebase.initializeApp(firebaseConfig);
